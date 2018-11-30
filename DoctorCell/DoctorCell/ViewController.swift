a7110434b3dffbd37bcf22a5fb2805b61c03416b
Gabriel Mocelin <gabriel.mocelin@acad.pucrs.br>
2018-11-30 10:48:42 -0200

    added empty project

---
+++ DoctorCell/DoctorCell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DoctorCell
+//
+//  Created by Gabriel Mocelin on 30/11/18.
+//  Copyright © 2018 Gabriel Mocelin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
